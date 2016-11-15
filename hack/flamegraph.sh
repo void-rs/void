77feb935105da8ce84c37c3f1f5bc62274d90b75
Tyler Neely <t@jujit.su>
2016-11-15 20:58:38 +0100

    reorg flamegraph stuff

--- hack/flamegraph.sh
+++ hack/flamegraph.sh
@@ -5,5 +5,5 @@ PID=$(pgrep climate)
 perf record -F 99 -p "$PID" -g -- sleep 60
 perf script > out.perf
 ./stackcollapse-perf.pl out.perf > out.folded
-./flamegraph.pl out.folded > out.svg
+./flamegraph.pl out.folded > flamegraph.svg
 rm perf.data out.perf out.folded
