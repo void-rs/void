9749cb63c6628cb3b2390190319a7f3c143b4ab7
Alok Singh <alokbeniwal@gmail.com>
2019-02-15 04:07:45 -0800

    Use lazy static for vector
    Probably unnecessary, but no sense in constructing a static vector twice.

--- src/colors.rs
+++ src/colors.rs
@@ -2,17 +2,19 @@ use rand::{self, Rng};
 
 pub fn random_fg_color() -> String {
     use termion::color::*;
-    let colors: Vec<String> = vec![
-        format!("{}", Fg(LightGreen)),
-        // format!("{}", Fg(LightBlack)),
-        format!("{}", Fg(LightRed)),
-        format!("{}", Fg(LightGreen)),
-        format!("{}", Fg(LightYellow)),
-        // format!("{}", Fg(LightBlue)),
-        format!("{}", Fg(LightMagenta)),
-        format!("{}", Fg(LightCyan)),
-        format!("{}", Fg(LightWhite)),
-    ];
-    let c = &*rand::thread_rng().choose(&*colors).unwrap();
-    c.clone()
+    lazy_static! {
+        static ref COLORS: Vec<String> = vec![
+            format!("{}", Fg(LightGreen)),
+            // format!("{}", Fg(LightBlack)),
+            format!("{}", Fg(LightRed)),
+            format!("{}", Fg(LightGreen)),
+            format!("{}", Fg(LightYellow)),
+            // format!("{}", Fg(LightBlue)),
+            format!("{}", Fg(LightMagenta)),
+            format!("{}", Fg(LightCyan)),
+            format!("{}", Fg(LightWhite)),
+        ];
+    }
+    let c = rand::thread_rng().choose(&COLORS).unwrap();
+    c.to_owned()
 }
