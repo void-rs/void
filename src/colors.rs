62d03411030ce4ae24c40afe4f639b793c42669d
Aleksi Hannula <ahannula4@gmail.com>
2021-03-04 14:17:55 +0200

    Use colours that are better suitable for light theme

--- src/colors.rs
+++ src/colors.rs
@@ -4,14 +4,23 @@ pub fn random_fg_color() -> String {
     use termion::color::*;
     let colors: Vec<String> = vec![
         format!("{}", Fg(LightGreen)),
-        // format!("{}", Fg(LightBlack)),
+        format!("{}", Fg(LightBlack)),
         format!("{}", Fg(LightRed)),
         format!("{}", Fg(LightGreen)),
-        format!("{}", Fg(LightYellow)),
-        // format!("{}", Fg(LightBlue)),
+        //format!("{}", Fg(LightYellow)),
+        format!("{}", Fg(LightBlue)),
         format!("{}", Fg(LightMagenta)),
         format!("{}", Fg(LightCyan)),
-        format!("{}", Fg(LightWhite)),
+        //format!("{}", Fg(LightWhite)),
+        format!("{}", Fg(Green)),
+        format!("{}", Fg(Black)),
+        format!("{}", Fg(Red)),
+        format!("{}", Fg(Green)),
+        format!("{}", Fg(Yellow)),
+        format!("{}", Fg(Blue)),
+        format!("{}", Fg(Magenta)),
+        format!("{}", Fg(Cyan)),
+        //format!("{}", Fg(White)),
     ];
     colors.choose(&mut rand::thread_rng()).unwrap().clone()
 }
